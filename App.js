1f49239d6769f7651fa00085757efc5059fdf095
LalithaKeerthana <121080065+LalithaKeerthana@users.noreply.github.com>
2023-12-06 00:58:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+// App.js
+
+import React from 'react';
+import './App.css';
+import Weather from './Weather';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Weather App</h1>
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
+
+
